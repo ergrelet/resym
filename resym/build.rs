5f93e348b061b1adc28a6dade4178b782b111cdf
ergrelet <ergrelet@users.noreply.github.com>
2023-02-18 17:04:34 +0100

    Add an icon to resym on Windows

---
+++ resym/build.rs
@@ -0,0 +1,27 @@
+#[cfg(windows)]
+fn main() {
+    // Convert ICO to raw pixels at build time. This avoids adding the `image` crate in `resym`
+    // only to display an icon.
+    let icon_bytes_path = std::path::Path::new(concat!(
+        env!("CARGO_MANIFEST_DIR"),
+        "/resources/resym_96.bin"
+    ));
+    // Only generate the binary file if it doesn't exist
+    if !icon_bytes_path.exists() {
+        let image_path = concat!(env!("CARGO_MANIFEST_DIR"), "/resources/resym_96.ico");
+        let image = image::open(image_path)
+            .expect("failed to open 96x96 ICO")
+            .into_rgba8();
+        let image_bytes = image.into_raw();
+
+        std::fs::write(icon_bytes_path, image_bytes).expect("failed to save 96x96 icon bytes");
+    }
+
+    // Compile the resource file with metadata and an icon
+    let mut res = winres::WindowsResource::new();
+    res.set_icon("resources/resym_256.ico");
+    res.compile().expect("Windows resource compilation failed");
+}
+
+#[cfg(unix)]
+fn main() {}
--- resym/src/main.rs
+++ resym/src/main.rs
@@ -34,13 +34,42 @@ const PDB_DIFF_SLOT: PDBSlot = 1;
 
 fn main() -> Result<()> {
     let logger = MemoryLogger::setup(log::Level::Info)?;
-    let native_options = eframe::NativeOptions::default();
+
+    let native_options = eframe::NativeOptions {
+        icon_data: load_icon(),
+        ..Default::default()
+    };
+
     eframe::run_native(
         PKG_NAME,
         native_options,
         Box::new(|cc| Box::new(ResymApp::new(cc, logger).expect("application creation"))),
     )
     .map_err(|err| anyhow!("eframe::run_native failed: {err}"))
+}
+
+/// Load an icon to display on the application's windows.
+/// Note: only available on Windows
+#[cfg(windows)]
+fn load_icon() -> Option<eframe::IconData> {
+    const ICON_WIDTH: u32 = 96;
+    const ICON_HEIGHT: u32 = 96;
+    const ICON_BYTES_PER_PIXEL: usize = 4;
+    const ICON_BYTE_SIZE: usize = ICON_WIDTH as usize * ICON_HEIGHT as usize * ICON_BYTES_PER_PIXEL;
+    const ICON_BYTES: &[u8; ICON_BYTE_SIZE] = include_bytes!("../resources/resym_96.bin");
+
+    Some(eframe::IconData {
+        rgba: ICON_BYTES.to_vec(),
+        width: ICON_WIDTH,
+        height: ICON_HEIGHT,
+    })
+}
+
+/// Load an icon to display on the application's windows.
+/// Note: only available on Windows
+#[cfg(not(windows))]
+fn load_icon() -> Option<eframe::IconData> {
+    None
 }
 
 /// Struct that represents our GUI application.
